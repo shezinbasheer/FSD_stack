4ad16df2859e292839a8540c09e00be490a18cd6
shezinbasheer <116833920+shezinbasheer@users.noreply.github.com>
2022-10-29 13:28:02 +0300

    Pre work uploaded

---
+++ DataTypeDemo.java
@@ -0,0 +1,41 @@
+package DataType;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ byte NumberOfmembersofafamily = 5;
+ System.out.println(NumberOfmembersofafamily);
+ short average = 113;
+ System.out.println( average);
+ int MarksScored = 6500;
+ System.out.println(MarksScored);
+ long PopulationOfIndia = 4500000000L;
+ System.out.println(PopulationOfIndia);
+ long populationOfIndia = 45_6500_900L;
+ System.out.println(populationOfIndia);
+		 
+ float PI = 3.14f;
+ System.out.println(PI);
+ float f2 = 1.23_4565F;
+ System.out.println(f2);
+ double demochallenge = 55.900060;
+ System.out.println(demochallenge);
+ char alphabet = 'A';
+ System.out.println(alphabet);
+ int tempint = (int)alphabet;
+ System.out.println(tempint);
+ tempint = tempint + 2;
+ alphabet = (char)tempint;
+ System.out.println(alphabet);
+ boolean matchwon = false;
+ matchwon = true; 
+ boolean b1 = (100>50);
+ System.out.println(b1);
+ boolean b2 = (100<50);
+ System.out.println(b2);
+ 
+ 
+	}
+
+}
